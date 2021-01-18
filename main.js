6776dac6625f77d7bf1a0021e43fe26cf759d704
DESKTOP-PITDT36\Rixi <sugarixi@gmail.com>
2021-01-18 22:09:27 +0200

    .

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
+//Javascriptin kirjoituksessa sain paljon ulkopuolista apua
+
+//lataa scriptin vasta, kunnes koko sivu on ladannut
 $(document).ready(function() {
 
+  //init sparkles, added 0.5 seconds delay so all the images will finish loading before executing --TÄTÄ OSIOTA EN OLE KIRJOITTANUT
   setTimeout(function() {
     $(".sparkles").sparkleh({always:false});
     $(".sparkles-always").sparkleh({always:true});
   }, 500);
 
-
-  //init socials toggle
-  $("#hrfSocials").click(function() {
-    $(".Socials").slideToggle( "slow", function() {
-      // Animation complete.
-    });
-  });
-
-  //init contact me toggle
+  //Click event listener jotta Contact me popuppi avautuu ja sulkeutuu
   $("#hrfContactMe").click(function() {
     $("#myModal")[0].style.display = "block";
   });
@@ -21,11 +17,15 @@ $(document).ready(function() {
     $("#myModal")[0].style.display = "none";
   });
 
+  //Muuttuja joka tallentaa nettisivun nykyisen tilan (light/dark)
   var curWebsiteMode = "Light";
-  //init light/dark toggle
+
+  //Click event listener, jotta light/dark mode togglaus toimii
   $("#ColorsToggle").click(function() {
-    $("#ModeText").html(curWebsiteMode + " Mode")
-    var oldMode = curWebsiteMode;
+    $("#ModeText").html(curWebsiteMode + " Mode"); //vaihtaa napin tekstin (light/dark)
+    var oldMode = curWebsiteMode; //Muuttuja joka tallentaa nettisivun edellisen tilan
+
+    //vaihtaa "<link rel="stylesheet" href="lightVars.css">" lightVars nimen darkVars:iksi
     if (curWebsiteMode == "Light") {
       $("link")[3].href = $("link")[3].href.replace("lightVars","darkVars")
       curWebsiteMode = "Dark";
@@ -35,6 +35,7 @@ $(document).ready(function() {
       curWebsiteMode = "Light";
     }
 
+    //Vaihtaa kaikkien kuvien nimet xxxLight.png -> xxxDark.png
     $.each($("img"), function(index, value) {
       value.src = value.src.replace(oldMode + ".png", curWebsiteMode + ".png")
     });
