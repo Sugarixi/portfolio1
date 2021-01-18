d70a1a944e99def5dfad4c34200956c371b3e536
DESKTOP-PITDT36\Rixi <sugarixi@gmail.com>
2021-01-18 16:39:31 +0200

    project files

---
+++ main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  //init "loading"
+  setTimeout(function() {
+    $(".websiteContent").toggleClass("noDisplay");
+    $(".stage").toggleClass("noDisplay");
+    $(".sparkles").sparkleh({always:false});
+      $(".sparkles-always").sparkleh({always:true});
+  }, 0)
+
+  //init socials toggle
+  $("#hrfSocials").click(function() {
+    $(".Socials").slideToggle( "slow", function() {
+      // Animation complete.
+    });
+  });
+
+  //init contact me toggle
+  $("#hrfContactMe").click(function() {
+    $("#myModal")[0].style.display = "block";
+  });
+  $(".close").click(function() {
+    $("#myModal")[0].style.display = "none";
+  });
+
+  var curWebsiteMode = "Light";
+  //init light/dark toggle
+  $("#ColorsToggle").click(function() {
+    $("#ModeText").html(curWebsiteMode + " Mode")
+    var oldMode = curWebsiteMode;
+    if (curWebsiteMode == "Light") {
+      $("link")[3].href = $("link")[3].href.replace("lightVars","darkVars")
+      curWebsiteMode = "Dark";
+    }
+    else {
+      $("link")[3].href = $("link")[3].href.replace("darkVars","lightVars")
+      curWebsiteMode = "Light";
+    }
+
+    $.each($("img"), function(index, value) {
+      value.src = value.src.replace(oldMode + ".png", curWebsiteMode + ".png")
+    });
+
+  });
+  
+});
